c6e148cd8f4342316fded565b7f27d7a804ea7ad
furtado3g <lucas_shiguioka@hotmail.com>
2020-02-19 21:55:36 -0300

    added template to chat

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express');
-const http = require('http').createServer(express());
-const io = require('socket-io');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = 3333;
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    return res.sendFile(__dirname+'/src/public/templates/chat.html');
+})
+
+io.on('connect',(socket)=>{
+    socket.on('message',(dados)=>{
+        socket.broadcast.emit('message',dados)
+        socket.emit('message',dados)
+    })
+})
+
+http.listen(port,()=>{
+    console.log("listen on "+port)
+})
